655d5060414eee1bea6299d6f9e10df424ac1aec
joallisson <joallissonnc@gmail.com>
2023-08-01 20:33:39 -0300

    #13 - Exibir Detalhes do Registro com Laravel

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 Route::get('/contato', [SiteController::class, 'contact']);
